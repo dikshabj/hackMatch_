3d638cb6fe177ae6ae5b356431f02d5e3ca67a29
bluebead421@gmail.com <bluebead421@gmail.com>
2026-05-11 10:41:21 +0530

    fix: add missing imports for GeneratedValue in Role entity

--- HackMatch_backend/auth-app-backend/auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/entities/Role.java
+++ HackMatch_backend/auth-app-backend/auth-app-backend/src/main/java/com/substring/auth/auth_app_backend/entities/Role.java
@@ -2,6 +2,8 @@ package com.substring.auth.auth_app_backend.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
